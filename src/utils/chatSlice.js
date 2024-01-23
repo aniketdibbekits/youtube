8fd24445bfb41cfb273e50d2683600bcb740c239
aniket <aniketdibbe75@gmail.com>
2024-01-23 22:56:32 +0530

    offset value

--- src/utils/chatSlice.js
+++ src/utils/chatSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { offset } from "./constants";
 
 const chatSlice=createSlice({
     name:'chat',
@@ -7,6 +8,8 @@ const chatSlice=createSlice({
     },
     reducers:{
         addMessage:(state,action)=>{
+            if(state.messages.length)
+            state.messages.splice(offset,1);
             state.messages.unshift(action.payload);
         },
     },
